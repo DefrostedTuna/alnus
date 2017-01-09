c53cebba10e69f716e388ac8d936889eb321581d
DefrostedTuna <rbennett1106@gmail.com>
2017-01-09 11:53:40 -0500

    Changed foreign key for users to uuid.

--- src/resources/migrations/2017_01_08_042029_create_roles_tables.php
+++ src/resources/migrations/2017_01_08_042029_create_roles_tables.php
@@ -41,7 +41,7 @@ class CreateRolesTables extends Migration
 
         Schema::create('role_user', function (Blueprint $table) {
             $table->integer('role_id')->unsigned();
-            $table->integer('user_id')->unsigned();
+            $table->uuid('user_id');
 
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
